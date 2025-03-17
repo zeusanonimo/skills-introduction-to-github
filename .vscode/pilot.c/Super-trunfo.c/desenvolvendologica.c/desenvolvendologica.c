2441d77311304236f1e1ed5b4c17ec0c71aa8043
zeusanonimo <rafaellgunner@gmail.com>
2025-03-17 05:02:15 +0000

    exercicio

---
+++ .vscode/pilot.c/Super-trunfo.c/desenvolvendologica.c/desenvolvendologica.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+  int main(){
+  
+   int numero1, numero2;
+    numero1 = 10;
+    numero2 = 5;
+  
+    if (numero1 == numero2){ printf("O numero 1 é igual a o numero 2\n");
+  
+    } else if(numero1>numero2) { printf("O numero 1 é maior que o numero 2.\n");
+    
+    } else{ printf("O numero 1 é menor que o numero 2.");}
+
+
+
+ return 0;}
+       
