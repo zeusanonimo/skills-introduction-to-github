154f889a95f82cf148cfbde3b9179cefe089fded
zeusanonimo <rafaellgunner@gmail.com>
2025-05-22 13:34:02 +0000

    habilidades especiais.

--- desafio.c/modificadoresdetipo/desafiio de xadrez.c/Movimentando as peças do xadrez.c/desafio batalhia naval.c/batalha naval.c
+++ desafio.c/modificadoresdetipo/desafiio de xadrez.c/Movimentando as peças do xadrez.c/desafio batalhia naval.c/batalha naval.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 #define TAMANHO 10  // Define o tamanho do tabuleiro
-#define NAVIO 3      // Valor que representa um navio no tabuleiro
+#define HABILIDADE 5 // Valor que representa uma área de efeito
 
+// Função para inicializar o tabuleiro com água (0)
 void inicializarTabuleiro(int tabuleiro[TAMANHO][TAMANHO]) {
-    // Inicializa o tabuleiro preenchendo com água (0)
     for(int i = 0; i < TAMANHO; i++) {
         for(int j = 0; j < TAMANHO; j++) {
             tabuleiro[i][j] = 0;
@@ -12,34 +12,44 @@ void inicializarTabuleiro(int tabuleiro[TAMANHO][TAMANHO]) {
     }
 }
 
-void posicionarNavios(int tabuleiro[TAMANHO][TAMANHO]) {
-    // Posiciona um navio horizontalmente
-    int linha_horizontal = 2, coluna_horizontal = 4;  
-    for(int i = 0; i < 3; i++) {
-        tabuleiro[linha_horizontal][coluna_horizontal + i] = NAVIO;
-    }
-
-    // Posiciona um navio verticalmente
-    int linha_vertical = 6, coluna_vertical = 7;  
-    for(int i = 0; i < 3; i++) {
-        tabuleiro[linha_vertical + i][coluna_vertical] = NAVIO;
-    }
-
-    // Posiciona um navio diagonal crescente (\)
-    int linha_diagonal1 = 1, coluna_diagonal1 = 1;
-    for(int i = 0; i < 3; i++) {
-        tabuleiro[linha_diagonal1 + i][coluna_diagonal1 + i] = NAVIO;
-    }
-
-    // Posiciona um navio diagonal decrescente (/)
-    int linha_diagonal2 = 5, coluna_diagonal2 = 8;
-    for(int i = 0; i < 3; i++) {
-        tabuleiro[linha_diagonal2 + i][coluna_diagonal2 - i] = NAVIO;
+// Função para posicionar habilidades no tabuleiro
+void posicionarHabilidade(int tabuleiro[TAMANHO][TAMANHO], int linha, int coluna, int tipo) {
+    switch(tipo) {
+        case 1: // Cone
+            for(int i = 0; i < 3; i++) {
+                for(int j = -i; j <= i; j++) {
+                    if (linha + i < TAMANHO && coluna + j >= 0 && coluna + j < TAMANHO) {
+                        tabuleiro[linha + i][coluna + j] = HABILIDADE;
+                    }
+                }
+            }
+            break;
+        case 2: // Cruz
+            for(int i = -2; i <= 2; i++) {
+                if (linha + i >= 0 && linha + i < TAMANHO) {
+                    tabuleiro[linha + i][coluna] = HABILIDADE;
+                }
+                if (coluna + i >= 0 && coluna + i < TAMANHO) {
+                    tabuleiro[linha][coluna + i] = HABILIDADE;
+                }
+            }
+            break;
+        case 3: // Octaedro
+            for(int i = -2; i <= 2; i++) {
+                for(int j = -2; j <= 2; j++) {
+                    if (abs(i) + abs(j) <= 2) {
+                        if (linha + i >= 0 && linha + i < TAMANHO && coluna + j >= 0 && coluna + j < TAMANHO) {
+                            tabuleiro[linha + i][coluna + j] = HABILIDADE;
+                        }
+                    }
+                }
+            }
+            break;
     }
 }
 
+// Função para exibir o tabuleiro
 void exibirTabuleiro(int tabuleiro[TAMANHO][TAMANHO]) {
-    // Exibe o tabuleiro na tela
     printf("Tabuleiro:\n");
     for(int i = 0; i < TAMANHO; i++) {
         for(int j = 0; j < TAMANHO; j++) {
@@ -53,7 +63,12 @@ int main() {
     int tabuleiro[TAMANHO][TAMANHO];
 
     inicializarTabuleiro(tabuleiro);
-    posicionarNavios(tabuleiro);
+    
+    // Posicionando habilidades especiais
+    posicionarHabilidade(tabuleiro, 3, 3, 1); // Cone em (3,3)
+    posicionarHabilidade(tabuleiro, 5, 5, 2); // Cruz em (5,5)
+    posicionarHabilidade(tabuleiro, 7, 7, 3); // Octaedro em (7,7)
+
     exibirTabuleiro(tabuleiro);
 
     return 0;
